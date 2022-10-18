07a97d0ae520eddf1029bb20282365a10031fefd
Poul Kjeldager Sørensen <pks@delegate.dk>
2022-10-18 08:19:12 +0200

    feat: added Pipelines and code

---
+++ src/EAVFW.Extensions.CommandLine/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.CommandLine.Parsing;
+
+namespace System.CommandLine
+{
+    public static class Extensions
+    {
+        public static T GetValue<T>(this Argument<T> argument, ParseResult parser)
+            => parser.GetValueForArgument(argument);
+        public static T GetValue<T>(this Option<T> argument, ParseResult parser)
+           => parser.GetValueForOption(argument);
+    }
+}
